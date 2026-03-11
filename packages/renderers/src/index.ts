5e20f115e92a295875775156fede674f59427b73
Deva Annamaraju <yansh2017@gmail.com>
2026-03-11 22:21:00 +0530

    chore: add TerminalRenderer export after merge

--- packages/renderers/src/index.ts
+++ packages/renderers/src/index.ts
@@ -1,3 +1,4 @@
 export type { IRenderer } from './renderer';
 export { JsonRenderer } from './json/index';
 export { SvgRenderer } from './svg/index';
+export { TerminalRenderer } from './terminal/index';
